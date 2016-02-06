4a251c3f69d3a9e45fc20ba1e8a41ed7f0df98b0
Brenton Klik <brenton.klik@gmail.com>
2016-02-06 17:55:25 -0500

    Update popupDirective.js
    Code refactor for readability.
    Better positioning.
    Fixed z-indexing.
    Fixed touch handling of hiding popup.

--- popupDirective.js
+++ popupDirective.js
@@ -1,17 +1,17 @@
 /***********************************************************************
- * Popup Directive
- * Author: Brenton Klik
- * 
- * Prerequisites:
- *  - AngularJS
- *  - styleSheetFactory (https://github.com/bklik/styleSheetFactory)
- * 
- * Description:
- * Create a popup positioned under the given element.
-/**********************************************************************/
-angular.module('popupDirective', ['styleSheetFactory'])
+Popup Directive
+Author: Brenton Klik
 
-.directive('popupDirective', ['styleSheetFactory', function(styleSheetFactory) {
+Prerequisites:
+ - AngularJS
+ - styleSheetFactory (https://github.com/bklik/styleSheetFactory)
+
+Description:
+Create a popup positioned under the given element.
+/**********************************************************************/
+angular.module('popup-directive', ['style-sheet-factory'])
+
+.directive('popup', ['styleSheetFactory', function(styleSheetFactory) {
     return {
         scope: {
             api: '=',
@@ -19,14 +19,134 @@ angular.module('popupDirective', ['styleSheetFactory'])
         },
         restrict: 'E',
         link: function($scope, $element, $attrs) {
+            /************************************************************************
+            API
+            ************************************************************************/
+            $scope.api = {
+                show: function(event) {
+                    show(event);
+                },
+
+                hide: function() {
+                    hide();
+                },
+            };
+
+            /************************************************************************
+            Variables
+            ************************************************************************/
             // The document's stylesheet.
             var styleSheet = styleSheetFactory.getStyleSheet();
 
             // The prefix used by the browser for non-standard properties.
             var prefix = styleSheetFactory.getPrefix();
 
+            // Target element the popup should appear next to.
+            var target = null;
+
+            // Used to track whether or not a move happened during a touch.
+            var touchMove = false;
+
+            // Used by event listeners to prevent the popup from closing.
+            var preventClose = function(event) {
+                event.stopPropagation();
+            }
+
+            /************************************************************************
+            Methods
+            ************************************************************************/
+            // Closes the popup if a touch happens and no touchmove event fired.
+            var touchHandler = function(event) {
+                if(event.type === "touchmove") {
+                    touchMove = true;
+                } else if(event.type === 'touchend') {
+                    if(!touchMove) {
+                        hide();
+                    }
+                    touchMove = false;
+                }
+            };
+
+            // Display the popup
+            var show = function(event) {
+                if(typeof event !== 'undefined') {
+                    if(typeof event.target !== 'undefined') {
+                        target = event.target;
+                    } else {
+                        target = event;
+                    }
+
+                    $element.addClass('show');
+                    position(target);
+
+                    target.addEventListener('mousedown', preventClose);
+                    target.addEventListener('keydown', hide);
+                    window.addEventListener('mousedown', hide);
+                    window.addEventListener('touchmove', touchHandler);
+                    window.addEventListener('touchend', touchHandler, false);
+                } else {
+                    console.error('Popup Directive method "show" requires a target element.');
+                }
+            };
+
+            // Hide the popup
+            var hide = function() {
+                if(typeof $scope.hideCallback == 'function') {
+                    $scope.hideCallback();
+                };
+
+                $element.removeClass('show');
+                target.removeEventListener('mousedown', preventClose);
+                target.removeEventListener('keydown', hide);
+                window.removeEventListener('mousedown', hide);
+                window.removeEventListener('touchmove', touchHandler);
+                window.removeEventListener('touchend', touchHandler);
+
+                target = null;
+                $element.removeClass('adjust-arrow');
+                styleSheetFactory.removeCSSRule(styleSheet, 'popup.adjust-arrow::after');
+            }
+
+            // Position the popup under the element that triggered the event.
+            var position = function(target) {
+                var targetRect = target.getBoundingClientRect();
+                var popupRect = $element[0].getBoundingClientRect();
+                var bodyRect = document.body.getBoundingClientRect();
+                var parentRect = target.parentNode.getBoundingClientRect();
+
+                var top = (targetRect.top - parentRect.top) + targetRect.height + 16;
+                var left = (targetRect.left - parentRect.left);
+                
+                // Make sure the popup isn't off the edge of the page.
+                if(targetRect.left + popupRect.width > bodyRect.width) {
+                    var adjustment = ((targetRect.left + popupRect.width) - bodyRect.width);
+                    left = left - adjustment;
+
+                    styleSheetFactory.addCSSRule(styleSheet, 'popup.adjust-arrow::after',
+                        'left: '+(10 + adjustment)+'px;'
+                    );
+                    $element.addClass('adjust-arrow');
+                }
+
+                $element.attr('style', 
+                    'top: '+top+'px;' +
+                    'left: '+left+'px;'
+                );
+            };
+
+            /************************************************************************
+            Init
+            ************************************************************************/
+            // Prevent close if any touchs/clicks happen inside the popup
+            $element.bind('mousedown', preventClose);
+            $element[0].addEventListener('touchend', preventClose, true);
+
+            /************************************************************************
+            Styles
+            ************************************************************************/
+
             // Add this directive's styles to the document's stylesheet.
-            styleSheetFactory.addCSSRule(styleSheet, 'popup-directive',
+            styleSheetFactory.addCSSRule(styleSheet, 'popup',
                 'background: white;' +
                 'border: 1px solid gray;' +
                 'border-radius: 2px;' +
@@ -34,11 +154,13 @@ angular.module('popupDirective', ['styleSheetFactory'])
                 'display: none;' +
                 'min-height: 32px;' +
                 'min-width: 32px;' +
+                'overflow: visible;' +
                 'padding: 8px;' +
-                'position: absolute;'
+                'position: absolute;' +
+                'z-index: 1;'
             );
 
-            styleSheetFactory.addCSSRule(styleSheet, 'popup-directive::after', 
+            styleSheetFactory.addCSSRule(styleSheet, 'popup::after', 
                 'background-color: white;' +
                 'border-left: 1px solid gray;' +
                 'border-top: 1px solid gray;' +
@@ -56,12 +178,7 @@ angular.module('popupDirective', ['styleSheetFactory'])
                 'transform: rotate(45deg);'
             );
 
-            styleSheetFactory.addCSSRule(styleSheet, 'popup-directive.arrow-right::after',
-                'left: auto;' +
-                'right: 16px;'
-            );
-
-            styleSheetFactory.addCSSRule(styleSheet, 'popup-directive.show',
+            styleSheetFactory.addCSSRule(styleSheet, 'popup.show',
                 'display: inline-block;' +
                 '' +
                 '-webkit-animation: popup-slidein 250ms;' +
@@ -72,8 +189,8 @@ angular.module('popupDirective', ['styleSheetFactory'])
             styleSheetFactory.addCSSKeyframes(styleSheet, 'popup-slidein',
                 'from {' +
                     'opacity: 0;' +
-                    '-'+prefix+'-transform: translateY(-.5em);' +
-                    'transform: translateY(-.5em);' +
+                    '-'+prefix+'-transform: translateY(16px);' +
+                    'transform: translateY(16px);' +
                 '}' +
                 'to {' +
                     'opacity: 1;' +
@@ -81,82 +198,6 @@ angular.module('popupDirective', ['styleSheetFactory'])
                     'transform: translateY(0);' +
                 '}'
             );
-
-            // Target element the popup should appear next to.
-            var target = null;
-
-            // Used by event listeners to prevent the popup from closing.
-            var preventClose = function(event) {
-                event.stopPropagation();
-            }
-
-            $element.bind('mousedown', preventClose);
-
-            var show = function(event) {
-                if(typeof event !== 'undefined') {
-                    if(typeof event.target !== 'undefined') {
-                        target = event.target;
-                    } else {
-                        target = event;
-                    }
-
-                    $element.addClass('show');
-                    position(target);
-
-                    target.addEventListener('mousedown', preventClose);
-                    target.addEventListener('keydown', hide);
-                    window.addEventListener('mousedown', hide);
-                } else {
-                    console.error('Popup Directive method "show" requires a target element.');
-                }
-            };
-
-            var hide = function() {
-                if(typeof $scope.hideCallback == 'function') {
-                    $scope.hideCallback();
-                };
-
-                $element.removeClass('show');
-                target.removeEventListener('mousedown', preventClose);
-                target.removeEventListener('keydown', hide);
-                window.removeEventListener('mousedown', hide);
-                target = null;
-            }
-
-            var position = function(target) {
-                var targetRect = target.getBoundingClientRect();
-                var popupRect = $element[0].getBoundingClientRect();
-                var bodyRect = document.body.getBoundingClientRect();
-
-                var top = targetRect.top + targetRect.height + 16;
-                var left = targetRect.left;
-                
-                if(targetRect.left + popupRect.width > bodyRect.width) {
-                    left = (targetRect.left + targetRect.width) - popupRect.width;
-
-                    if(left > 0) {
-                        $element.addClass('arrow-right');
-                    } else {
-                        left = targetRect.left;
-                        $element.removeClass('arrow-right');
-                    }
-                }
-
-                $element.attr('style', 
-                    'top: '+top+'px;' +
-                    'left: '+left+'px;'
-                );
-            };
-
-            $scope.api = {
-                show: function(event) {
-                    show(event);
-                },
-
-                hide: function() {
-                    hide();
-                },
-            };
         }
     }
 }]);
